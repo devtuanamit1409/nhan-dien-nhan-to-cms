8386f6ee2c97fa82514cace34613af59b82cbb89
devtuanamit1409 <nguyentuanamit@gmail.com>
2024-11-01 13:29:54 +0700

    fix: fix export excel

--- config/excel.js
+++ config/excel.js
@@ -5,6 +5,14 @@ module.exports = {
       // UID của collection type
       columns: [
         "type", // Tên cột muốn xuất
+        "name",
+        "address",
+        "email",
+        "age",
+        "phone",
+        "birthDate",
+        "position",
+        "Tags",
       ],
       relation: {}, // Không có quan hệ nào trong collection này, nên để trống
       locale: "false", // Đặt "false" vì không có cấu hình i18n
